303c688ea26d75d27bc81c66f408c0ec31be21e5
vinodaws2020 <vinodaws2020@gmail.com>
2026-01-11 09:29:50 +0530

    terraform update

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.webapps.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.webapps.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.webapps_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.webapps_subnet[*].id
 }
 
